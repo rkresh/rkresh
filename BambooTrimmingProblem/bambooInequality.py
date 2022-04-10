9cdb7eb553d58657584dc926bb45159e37353efa
Reshma Kuriachan <103388017+rkresh@users.noreply.github.com>
2022-04-10 23:16:05 +0100

    Add files via upload

---
+++ BambooTrimmingProblem/bambooInequality.py
@@ -0,0 +1,26 @@
+subtaskId = 1
+growthRate = [98, 1, 1]
+
+# Dont change anything above this line
+# ===================================
+
+# enter your username for EVERY file you submit
+username = "MWS_USERNAME"
+
+# generate your solution as a list
+queue = [0, 1, 2]
+
+# ====================================
+# Dont change anything below this line
+
+from collections import deque
+
+solution = deque()
+# add each element to the solution
+for i in queue:
+    solution.append(i)
+
+import bamboo
+
+# records your solution
+bamboo.calculateRatio(growthRate, solution, username, subtaskId)
